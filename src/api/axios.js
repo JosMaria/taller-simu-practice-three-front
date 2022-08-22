08f71bae25389df836d8ae634c238dc0c0848ac7
Jose Maria <josmariaguilar@gmail.com>
2022-08-21 22:34:23 -0400

    Fetched data of back-end

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,7 @@ const URL = 'http://localhost:8080/api/v1/exercises';
 const dataOfMiddleSquare = async (seed = 5356, times = 5) => {
   const request = `${URL}/middleSquare?seed=${seed}&times=${times}`; 
   const response = await axios.get(request);
-  return response;
+  return response.data;
 };
 
 export {
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,53 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { dataOfMiddleSquare } from '../api/axios';
 import { Table } from './Table'
 
-const fake = [
-  {
-    "numberOfRow": 1,
-    "valueOne": 5356,
-    "valueTwo": 28686736,
-    "valueThree": "28686736",
-    "valueFour": "6867",
-    "valueFive": 0.6867
-  },
-  {
-    "numberOfRow": 2,
-    "valueOne": 6867,
-    "valueTwo": 47155689,
-    "valueThree": "47155689",
-    "valueFour": "1556",
-    "valueFive": 0.1556
-  },
-  {
-    "numberOfRow": 3,
-    "valueOne": 1556,
-    "valueTwo": 2421136,
-    "valueThree": "02421136",
-    "valueFour": "4211",
-    "valueFive": 0.4211
-  },
-  {
-    "numberOfRow": 4,
-    "valueOne": 4211,
-    "valueTwo": 17732521,
-    "valueThree": "17732521",
-    "valueFour": "7325",
-    "valueFive": 0.7325
-  },
-  {
-    "numberOfRow": 5,
-    "valueOne": 7325,
-    "valueTwo": 53655625,
-    "valueThree": "53655625",
-    "valueFour": "6556",
-    "valueFive": 0.6556
-  }
-];
-
-export const Content = () => {
+export const Content = () => {  
+  const [rows, setRows] = useState([]);
   
-  const [rows, setRows] = useState(fake);
-  
+  useEffect(() => {
+    dataOfMiddleSquare(5356, 5)
+      .then(data => setRows(data));
+  }, []);
 
   return (
     <Table rows={rows} />
