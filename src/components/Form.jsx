5af57b8810eb0775ec46969d3abae11fd5d09534
Jose Maria <josmariaguilar@gmail.com>
2022-08-23 11:39:06 -0400

    fixed validations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import { validationOfSeed, validationNumberOfIterations } from '../utils/validations';
 
 import '../stylesheets/Form.css';
 
@@ -13,7 +14,8 @@ export const Form = () => {
     <div className='input-data-container'>
       <label className='text-label'>Semilla</label>
       <input className='text-input' type='text' {...register('seed', {
-        required: true
+        required: true,
+        validate: validationOfSeed
       })} />
     </div>
 
@@ -21,18 +23,20 @@ export const Form = () => {
     <div className='input-data-container'>
       <label className='text-label'>Número de iteraciones</label>
       <input className='text-input' type='text' {...register('times', { 
-        required: true
+        required: true,
+        validate: validationNumberOfIterations
       })} />
     </div>
     
   const inputs = 
     <>
       { inputDataSeed }
-      { errors.seed?.type === 'required' && <p>campo obligatorio</p> }
+      { errors.seed?.type === 'required' && <p>semilla campo obligatorio</p> }
+      { errors.seed?.type === 'validate' && <p>Semilla tiene que tener mas de 3 digitos</p> }
       { inputDataTimes }
-      { errors.times?.type === 'required' && <p>campo obligatorio</p> }
+      { errors.times?.type === 'required' && <p>iteraciones campo obligatorio</p> }
+      { errors.seed?.type === 'validate' && <p>Numero de iteraciones tiene que ser mayor a 0</p> }
     </>
-    
 
   return (
     <form className='form-container' onSubmit={handleSubmit(customSubmit)}>
