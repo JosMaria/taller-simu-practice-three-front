6c2b1f3d8da91e94dd3ee075ca57613d2086cf39
Jose Maria <josmariaguilar@gmail.com>
2022-08-27 00:02:13 -0400

    Creation of icon menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,28 @@
+import { BiMenu } from 'react-icons/bi';
+
 import '../stylesheets/Menu.css';
 
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <nav className='menu-container'>
+      <div className='hamburguer'>
+        <BiMenu className='icon' />
+      </div>
+    </nav>
   )
 }
+
+/* 
+  GENEREADORES NO CONGRUENCIALES
+    ALGORITMO DE CUADRADOS MEDIOS
+    ALGORITMO DE PRODUCTOS MEDIOS
+    ''      ''    MULTIPLICADOR CONSTANTE
+
+  GENERADORES CONGRUENCIALES
+    CONGRUENCIAL MIXTO
+    CONGRUENCIAL MULTIPLICATIVO
+    CONGRUENCIAL ADITIVO
+    CUADRATICO
+    BLUM BLUM Y SHUB
+
+*/
