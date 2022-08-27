9c1b4a291f1cfa287dcf16c209d35b7703c05789
Jose Maria <josmariaguilar@gmail.com>
2022-08-27 10:40:03 -0400

    Refactoring and more refactoring

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,108 +1,49 @@
 import { useState } from 'react';
 import { BiMenu } from 'react-icons/bi';
-import { BsListNested } from "react-icons/bs";
-import { FiChevronRight } from 'react-icons/fi';
+import { MenuItem } from './MenuItem';
+import { ItemChildren } from './MenuItem';
 
 import '../stylesheets/Menu.css';
-import { MenuItem } from './MenuItem';
 
+const Data = () => {
+  const childrenFirstMenu = 
+    <>
+      <ItemChildren titleItem='Cuadrados Medios' to={'#'} />
+      <ItemChildren titleItem='Productos Medios' to={'#'} />
+      <ItemChildren titleItem='Multiplicador Constante' to={'#'} />
+    </>
+
+  const firstMenuItem = <MenuItem height={'5.1em'} children={childrenFirstMenu} />
+
+  const childrenSecondMenu = 
+    <>
+      <ItemChildren titleItem='Mixto' to={'#'} />
+      <ItemChildren titleItem='Multiplicativo' to={'#'} />
+      <ItemChildren titleItem='Aditivo' to={'#'} />
+      <ItemChildren titleItem='Cuadratico' to={'#'} />
+      <ItemChildren titleItem='Blum Blum y Shub' to={'#'} />
+    </>
+
+  const secondMenuItem = <MenuItem height={'8.6em'} children={childrenSecondMenu}/>
+  return (
+    <>
+      {firstMenuItem}
+      {secondMenuItem}
+    </>
+  )
+}
 
 export const Menu = () => {
-  const [itemCongruential, setItemCongruential] = useState({ clicked: false, height: '0' });
-  const [itemNonCongruential, setItemNonCongruential] = useState({ clicked: false, height: '0' });
   const [menuClicked, setMenuClicked] = useState(false);
   
-  const congruentialClicked = () => {
-    const updated = {}
-    updated.clicked = !itemCongruential.clicked;
-    updated.height = itemCongruential.clicked ? '0' : '8.5em';
-    setItemCongruential(updated);
-  }
-
-  /*const nonCongruentialClicked = () => {
-    const updated = {}
-    updated.clicked = !itemNonCongruential.clicked;
-    updated.height = itemNonCongruential.clicked ? '0' : '3.6em';
-    setItemNonCongruential(updated);
-  }*/
-
-  /* DATA ITEMS */
-
-  const heightMenuItem = '3.6em';
-  const [itemMenuClicked, setItemMenuClicked] = useState(false);
-  const [itemMenuHeight, setItemMenuHeight] = useState('0');
-
-  const menuClickedTemp = () => {
-    setItemMenuClicked(!itemMenuClicked);
-    setItemMenuHeight(itemMenuClicked ? '0' : heightMenuItem)
-  } 
-
   return (
     <nav className='menu__container'>
       <div className='hamburguer' onClick={() => setMenuClicked(!menuClicked)}>
         <BiMenu className='icon' />
       </div>
       <ul className={`list ${menuClicked ? 'translate' : ''}`}>
-        
-        <MenuItem height={'5.1em'} />
-        {/* <li className='list__item list__item--click' onClick={menuClickedTemp}>
-          <div className={`list__button list__button--click ${itemMenuClicked ? 'arrow' : ''}`}>
-            <BsListNested className='list__icon' />
-            <a herf='#' className='nav__link'>NO CONGRUENCIALES</a>
-            <FiChevronRight className='list__arrow' />
-          </div>
-          <ul className='list__show' style={{ height: itemMenuHeight }}>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Cuadrados Medios</a>
-            </li>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Productos Medios</a>
-            </li>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Multiplicador Constante</a>
-            </li>
-          </ul>
-        </li> */}
-        {/* <li className='list__item list__item--click' onClick={congruentialClicked}>
-          <div className={`list__button list__button--click ${itemCongruential.clicked ? 'arrow' : ''}`}>
-            <BsListNested className='list__icon' />
-            <a herf='#' className='nav__link'>CONGRUENCIALES</a>
-            <FiChevronRight className='list__arrow' />
-          </div>
-          <ul className='list__show' style={{ height: itemCongruential.height }}>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Mixto</a>
-            </li>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Multiplicativo</a>
-            </li>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Aditivo</a>
-            </li>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Cuadratico</a>
-            </li>
-            <li className='list__inside'>
-              <a href='#' className='nav__link nav__link--inside'>Blum Blum y Shub</a>
-            </li>
-          </ul>
-        </li> */}
+        <Data />
       </ul>
     </nav>
   )
 }
-
-/* 
-  GENEREADORES NO CONGRUENCIALES
-    ALGORITMO DE CUADRADOS MEDIOS
-    ALGORITMO DE PRODUCTOS MEDIOS
-    ''      ''    MULTIPLICADOR CONSTANTE
-
-  GENERADORES CONGRUENCIALES
-    CONGRUENCIAL MIXTO
-    CONGRUENCIAL MULTIPLICATIVO
-    CONGRUENCIAL ADITIVO
-    CUADRATICO
-    BLUM BLUM Y SHUB
-
-*/
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,6 +2,23 @@ import { BsListNested } from "react-icons/bs";
 import { FiChevronRight } from 'react-icons/fi';
 import { useState } from 'react';
 
+export const MenuItem = ({ height, children }) => {
+  const [itemMenuClicked, setItemMenuClicked] = useState(false);
+  const [itemMenuHeight, setItemMenuHeight] = useState('0');
+
+  const menuItemClicked = () => {
+    setItemMenuClicked(!itemMenuClicked);
+    setItemMenuHeight(itemMenuClicked ? '0' : height);
+  }
+  
+  return (
+    <li className='list__item list__item--click' onClick={menuItemClicked}>
+      <ItemAncestor titleName='NO CONGRUENCIALES' clickedAncestor={itemMenuClicked} />
+      <ItemChildrenList heightList={itemMenuHeight} children={children} />
+    </li>
+  )
+}
+
 const ItemAncestor = ({ titleName, clickedAncestor }) => {
   return (
     <div className={`list__button list__button--click ${clickedAncestor ? 'arrow' : ''}`}>
@@ -20,34 +37,10 @@ const ItemChildrenList = ({ heightList, children }) => {
   )
 };
 
-const ItemChildren = ({ titleItem }) => {
+export const ItemChildren = ({ titleItem, to }) => {
   return (
     <li className='list__inside'>
-      <a href='#' className='nav__link nav__link--inside'>{titleItem}</a>
+      <a href={to} className='nav__link nav__link--inside'>{titleItem}</a>
     </li>  
   ) 
 };
-
-export const MenuItem = ({ height }) => {
-  const [itemMenuClicked, setItemMenuClicked] = useState(false);
-  const [itemMenuHeight, setItemMenuHeight] = useState('0');
-
-  const menuItemClicked = () => {
-    setItemMenuClicked(!itemMenuClicked);
-    setItemMenuHeight(itemMenuClicked ? '0' : height);
-  }
-
-  const children = 
-    <>
-      <ItemChildren titleItem='Cuadrados Medios' /> 
-      <ItemChildren titleItem='Productos Medios' />
-      <ItemChildren titleItem='Multiplicador Constante' />
-    </>
-  
-  return (
-    <li className='list__item list__item--click' onClick={menuItemClicked}>
-      <ItemAncestor titleName='NO CONGRUENCIALES' clickedAncestor={itemMenuClicked} />
-      <ItemChildrenList heightList={itemMenuHeight} children={children} />
-    </li>
-  )
-}
