eb7295ff91d73c99d3e492f2b2608b7de03aafc8
Jose Maria <josmariaguilar@gmail.com>
2022-08-29 01:17:05 -0400

    Calling api to get data of constant multiplier

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,7 +6,7 @@ const domain = axios.create({
 
 const dataOfMiddleSquare = async (seed, times) => {
   if (seed !== 0 && times !== 0) {
-    const response = await domain.get(`api/middleSquare?seed=${seed}&times=${times}`)
+    const response = await domain.get(`api/middleSquare?seed=${seed}&times=${times}`);
     return response.data;
   }
   return [];
@@ -14,13 +14,22 @@ const dataOfMiddleSquare = async (seed, times) => {
 
 const dataOfAverageProduct = async (seedOne, seedTwo, times) => {
   if (seedOne !== 0 && seedTwo !== 0 && times !== 0) {
-    const response = await domain.get(`api/averageProduct?seedOne=${seedOne}&seedTwo=${seedTwo}&times=${times}`)
+    const response = await domain.get(`api/averageProduct?seedOne=${seedOne}&seedTwo=${seedTwo}&times=${times}`);
     return response.data;
   }
   return [];
 };
 
+const dataOfConstantMultiplier = async (seed, constant, times) => {
+  if (seed !== 0 && constant !== 0 && times !== 0) {
+    const response = await domain.get(`api/constantMultiplier?seed=${seed}&constant=${constant}&times=${times}`);
+    return response.data;
+  }
+  return [];
+}
+
 export {
   dataOfMiddleSquare,
-  dataOfAverageProduct
+  dataOfAverageProduct,
+  dataOfConstantMultiplier
 }
--- src/pages/ConstantMultiplier.jsx
+++ src/pages/ConstantMultiplier.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import { Table } from '../components/Table';
 import '../stylesheets/pages/ConstantMultiplier.css';
 
+const titleHeaders = ['n', 'a', 'X', 'a * X', 'Xn+1', 'Ri'];
+
 export const ConstantMultiplier = () => {
+  const [rows, setRows] = useState([]);
+
   return (
-    <div>ConstantMultiplier</div>
+    <div className='constant-multiplier-container'>
+      <h1>Multiplicador Constante</h1>
+      <Table titleHeaders={titleHeaders} rows={rows} />
+    </div>
   )
 }
